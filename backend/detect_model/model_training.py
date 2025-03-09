7f34cbae6d2ca136cecca67960a1c5ab2ec47dcd
Ahero234 <nitinvjacob@gmail.com>
2025-03-09 04:31:39 +0000

    updated training model to detect more accurately

--- backend/detect_model/model_training.py
+++ backend/detect_model/model_training.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression  # Changed to Logistic Regression
 from sklearn.metrics import accuracy_score, classification_report
 import pickle
 
@@ -53,8 +53,8 @@ X_vectorized = vectorizer.fit_transform(X)
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X_vectorized, y, test_size=0.3, random_state=42)
 
-# Model training
-model = GradientBoostingClassifier(n_estimators=300, learning_rate=0.05, random_state=42)
+# Model training with Logistic Regression
+model = LogisticRegression(max_iter=1000, random_state=42)  # Use Logistic Regression
 model.fit(X_train, y_train)
 
 # Evaluate the model
@@ -83,3 +83,63 @@ with open('backend/model.pkl', 'wb') as model_file:
 with open('backend/vectorizer.pkl', 'wb') as vectorizer_file:
     pickle.dump(vectorizer, vectorizer_file)
     print("Vectorizer saved")
+
+
+'''import string
+import numpy as np
+import pandas as pd
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+nltk.download('stopwords')
+
+df = pd.read_csv('data/Enron.csv')
+
+df['text'] = df['text'].apply(lambda x: x.replace('\r\n', ' '))
+df.info()
+
+stemmer = PorterStemmer()
+corpus = []
+
+stopwords_set = set(stopwords.words('english'))     
+
+for i in range(len(df)):
+    text = df['text'].iloc[i].lower()
+    text = text.translate(str.maketrans('', '', string.punctuation)).split()
+    text = [stemmer.stem(word) for word in text if word not in stopwords_set]
+    text = ''.join(text)
+    corpus.append(text)
+
+vectorizer = CountVectorizer()
+
+X = vectorizer.fit_transform(corpus).toarray()
+y = df.label
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+classifier = RandomForestClassifier(n_jobs=-1)
+classifier(X_train, y_train)
+
+classifier.score(X_test, y_test)
+
+email = df.text.values[10]
+
+email_text = email.lower().translate(str.maketrans('', '', string.punctuation)).split()
+email_text = [stemmer.stem(word) for word in email_text if word not in stopwords_set]
+email_text = ''.join(email_text)
+
+email_corpus = [email_text]
+
+X_email = vectorizer.transform(email_corpus)
+
+classifier.predict(X_email)
+
+df.label.iloc[10]'''
