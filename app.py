9de7180a3b8d49f5a119bdec7a70b9591dbb3777
Ahero234 <nitinvjacob@gmail.com>
2025-03-17 04:59:25 +0000

    added dark mode, high contrast and a text resizing toggle for accessibility

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lottery.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialise database
-# db = SQLAlchemy(app)
 
 # Blueprints
 from backend.detect_model.detect import detect_blueprint 
